797011fa5d9a7eb7a0d56d2c1fcefb3d778cdacb
ciruu1 <ciruu@protonmail.com>
2023-12-11 18:45:38 +0100

    Tested some components

--- SW/SMA_LAMP.X/Init/initall.c
+++ SW/SMA_LAMP.X/Init/initall.c
@@ -28,7 +28,40 @@ void init_uart(void)
 
 }
 
+void init_adc(void)
+{
+    ADCON0bits.ADCS1 = 1;
+    ADCON0bits.ADCS0 = 0;
+    ADCON0bits.CHS = 0b1100;
+    ADCON0bits.GO = 0;
+    ADCON0bits.ADON = 1;
+    ADCON1bits.ADFM = 1;
+    PIE1bits.ADIE = 1; // Activa la interrupcion en el ADC
+
+    ADCON1bits.VCFG1 = 0;
+    ADCON1bits.VCFG0 = 0;
+    TRISBbits.TRISB0 = 1;
+    ANSELHbits.ANS12 = 1;
+
+}
+
+void init_timer()
+{
+  OPTION_REGbits.T0CS = 0;
+  OPTION_REGbits.PSA = 0;
+  OPTION_REGbits.PS = 0b111; // Fosc/32
+  INTCONbits.T0IE = 1; // Habilita las interrupciones del timer0
+  INTCONbits.T0IF = 0;
+
+  TMR0 = 159;  // Cada 5 mseg   256-97  (5*10^6/ 256)*0.005 = 97.6 
+}
+
 void init(void)
 {
+    OSCCON = 0b00001000;
+    INTCONbits.GIE = 1;
+    init_timer();
     init_uart();
+    init_adc();
+    
 }
--- SW/SMA_LAMP.X/main.c
+++ SW/SMA_LAMP.X/main.c
@@ -7,14 +7,101 @@
 
 // TODO cambiar los nombres del diagrama de alto nivel de Español a Inglés
 
+
+#pragma config CPD = OFF, BOREN = OFF, IESO = OFF, DEBUG = OFF, FOSC = HS
+#pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
+#pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
+
 #include <xc.h>
 #include <proc/pic16f886.h>
+#include <stdio.h>
 
 // With this library we configure the main settings on the MCU
 #include "Init/initall.c"
+//#include "action.c"
+
+int counter;
+int counterRuido10ms, counterRuido1seg, counter5seg; // 2 ticks, 200 ticks, 1000 ticks
+int uart_num;
+int NoiseValue, HumValue, TempValue;
+char noiseValues[10]; 
+int noiseCounterValues; // 0..9
+int maxNoise = 0;
+
+void putch(char data)
+{
+    while (!TXIF);  // Espera hasta que el registro de transmisión esté listo para enviar
+        TXREG = data;  // Carga el carácter en el registro de transmisión
+}
+
+int readADC(int channel)
+{
+    ADCON0bits.CHS = channel; // AN8
+    ADCON0bits.GO = 1;
+    while(ADCON0bits.GO);
+    return ((ADRESH) << 8) | ADRESL;
+}
+
+void __interrupt() int_routine(void)
+{
+    if(INTCONbits.T0IF)
+    {
+        counter += 1;
+        counterRuido10ms += 1;
+        counterRuido1seg += 1;
+        counter5seg += 1;
+        TMR0 = 159;
+        INTCONbits.T0IF = 0;
+        if(counterRuido10ms >= 2) // NOISE
+        {
+            counterRuido10ms = 0;
+            NoiseValue = readADC(8); // AN8
+            if(noiseCounterValues >= 10)
+            {
+                noiseCounterValues = 0;
+            }
+            noiseValues[noiseCounterValues] = NoiseValue;
+            noiseCounterValues++;
+            // Leer ruido 10 ms
+        }
+        if(counterRuido1seg >= 200)
+        {
+            counterRuido1seg = 0;
+            maxNoise = 0;
+            for(int i = 0; i < 10; i++) {
+                if(noiseValues[i] >= maxNoise) {
+                    maxNoise = noiseValues[i];
+                }
+            }
+            printf("NOISE %u", maxNoise);
+        }
+        if(counter5seg >= 1000)
+        {
+            counter5seg = 0;
+            HumValue = readADC(12);
+            TempValue = readADC(10);
+            printf("HUMIDITY %u", HumValue);
+            printf("TEMPERATURE %u", TempValue);
+            // I2C
+            
+            // Leer Sensores
+        }
+    }
+    //UART
+
+}
 
 void main(void)
 {
     init(); // We call the initialize function in the init library 
+    counter = 0;
+    counterRuido10ms = 0;
+    counterRuido1seg = 0;
+    counter5seg = 0;
+    noiseCounterValues = 0;
+    while(1)
+    {
+        
+    }
     return;
 }
